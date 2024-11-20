31aa00b0f3973e1dacd8ebe06243319ec9f355f8
WhiredPlanck <whiredplanck@outlook.com>
2024-11-21 00:28:17 +0800

    refactor: rename InlinePreeditMode to ComposingTextMode and set DISABLED as default value

---
+++ app/src/main/java/com/osfans/trime/ime/core/ComposingTextMode.kt
@@ -0,0 +1,19 @@
+/*
+ * SPDX-FileCopyrightText: 2015 - 2024 Rime community
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+package com.osfans.trime.ime.core
+
+import androidx.annotation.StringRes
+import com.osfans.trime.R
+import com.osfans.trime.data.prefs.PreferenceDelegateEnum
+
+enum class ComposingTextMode(
+    @StringRes override val stringRes: Int,
+) : PreferenceDelegateEnum {
+    DISABLE(R.string.disable),
+    PREEDIT(R.string.preedit),
+    COMMIT_TEXT_PREVIEW(R.string.commit_text_preview),
+    RAW_INPUT(R.string.raw_input),
+}
